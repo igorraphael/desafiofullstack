0c0da9303eddc8bcb5c4e63c03bb84e46bdf65ed
Igor Raphael <igorraphaeel@live.com>
2019-11-07 17:17:42 -0300

    Acrescentado informações do usuario, tags e view_count.

--- server/src/schema.js
+++ server/src/schema.js
@@ -8,10 +8,18 @@ const typeDefs = gql`
     type Question {
         id: ID!
         title: String
+        tags: [String]
         link: String
         score: Int
+        view_count: Int
+        owner: User
     }
 
+    type User {
+        user_id: ID!
+        profile_image: String
+        display_name: String
+    }
 `;
 
 module.exports = typeDefs;
